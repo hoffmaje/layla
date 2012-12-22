ef66ad18c2a27f76715f63794a4fbd92e49e21a4
hoffmaje <jgk.hoffmann@gmail.com>
2012-12-22 12:32:13 +0100

    Models Language and Word + their relationship.

--- layla/vocabularymanager/models.py
+++ layla/vocabularymanager/models.py
@@ -1,3 +1,14 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Copyright 2012 Jens Hoffmann (hoffmaje)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    label = models.CharField(max_length=80)
+
+
+class Word(models.Model):
+    label = models.CharField(max_length=80)
+    language = models.ForeignKey(Language)
+

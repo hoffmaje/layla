a7a81d5062250c57607b3fa1a23bc1691844550e
hoffmaje <jgk.hoffmann@gmail.com>
2012-12-23 17:13:13 +0100

    Enabled the admin interface and the admin docs.

--- layla/urls.py
+++ layla/urls.py
@@ -2,20 +2,17 @@
 # Copyright 2012 Jens Hoffmann
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'layla.views.home', name='home'),
     # url(r'^layla/', include('layla.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^vocabularymanager/', include('layla.vocabularymanager.urls')),
     url(r'^', include('layla.main.urls')),
 )

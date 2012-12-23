9905a68a33400522a9b9a2296f322572e38251bd
hoffmaje <jgk.hoffmann@gmail.com>
2012-12-23 22:14:16 +0100

    Unicode methods for vocabulary manager models.

--- layla/vocabularymanager/models.py
+++ layla/vocabularymanager/models.py
@@ -7,8 +7,14 @@ from django.db import models
 class Language(models.Model):
     language = models.CharField(max_length=80)
 
+    def __unicode__(self):
+        return self.language
+
 
 class Phrase(models.Model):
     phrase = models.CharField(max_length=80)
     language = models.ForeignKey(Language)
+
+    def __unicode__(self):
+        return self.phrase
 

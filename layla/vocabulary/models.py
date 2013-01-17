0c50dc6f0ae109befb486a429c32aa94a5557161
hoffmaje <jgk.hoffmann@gmail.com>
2013-01-17 14:48:05 +0100

    Many phrases can have many translations (=phrases).

--- layla/vocabulary/models.py
+++ layla/vocabulary/models.py
@@ -6,15 +6,20 @@ from django.db import models
 
 class Language(models.Model):
     value = models.CharField(max_length=80)
+    country_code = models.CharField(max_length=2)
 
     def __unicode__(self):
         return self.value
 
 
 class Phrase(models.Model):
-    value = models.CharField(max_length=80)
+    class Meta:
+        ordering = ['language__country_code']
+
     language = models.ForeignKey(Language)
+    value = models.CharField(max_length=80)
+    translations = models.ManyToManyField('self', null=True, blank=True)
 
     def __unicode__(self):
-        return self.value
+        return "%s - %s" % (self.language.country_code, self.value)
 
